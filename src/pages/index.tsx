b66481f32246e599eb08173591d53f7b37334613
mufaqar <mufaqar@gmail.com>
2023-12-06 16:29:45 +0500

    updated data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,38 +9,38 @@ import { useEffect, useState } from "react";
 export default function Home() {
 
 
-      const [prayerTimings, setPrayerTimings] = useState(null);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(
-            'https://api.aladhan.com/v1/timingsByCity/06-12-2023?city=Valletta&country=Malta'
-          );
-  
-          if (!response.ok) {
-            throw new Error('Failed to fetch data');
-          }
-  
-          const data = await response.json();
-          setPrayerTimings(data.data);
-          setLoading(false);
-        } catch (error) {
-          console.error('Error:', error);
-          setLoading(false);
+  const [prayerTimings, setPrayerTimings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://api.aladhan.com/v1/timingsByCity?city=Valletta&country=Malta'
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
         }
-      };
-  
-      fetchData();
-    }, []);
+
+        const data = await response.json();
+        setPrayerTimings(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
 
   return (
     <main>
       <SeoMeta title="Malta Islamic Center | Muslim Guide Malta" description="A Place of Worship and Guidance, Where You Embrace the Beauty of Islamic Teachings." url="https://muslimsguidemalta.com/" />
       <Banner />
-      <Schedule  prayerTimings={prayerTimings} />
+      <Schedule prayerTimings={prayerTimings} />
       <Services />
       <Cta />
       <Pillar_Islam />
